c34cf48eac86afb698b44ae04e5c9da01ab96987
Anthony Cannistra <tony.cannistra@gmail.com>
2013-07-10 09:46:46 -0400

    added functionality for temperature, which is not yet available.

--- sail.py
+++ sail.py
@@ -1,17 +1,23 @@
 #! /usr/bin/env python
 
-import re
 import mechanize
 from BeautifulSoup import BeautifulSoup
+import json
+import urllib2
+from time import time
 
 SAILCBI = "https://portal.community-boating.org/apex/f?p=710:1"
+WEATHER = "https://api.forecast.io/forecast/7516bbf0f9e3cf4343c3744256fd183d/42.359439,-71.073260"
 
-resp    = mechanize.urlopen(SAILCBI)
-soup    = BeautifulSoup(resp.read())
+sail_cbi_resp = mechanize.urlopen(SAILCBI)
+sail_cbi_soup = BeautifulSoup(sail_cbi_resp.read())
+flagcolor = sail_cbi_soup.findAll('h2')[0].contents[2]
 
-flagcolor = soup.findAll('h2')[0].contents[2]
+#json   = json.loads(urllib2.urlopen(WEATHER).read())
 
-print "flag: "+flagcolor
+print "flag: "+flagcolor+' |',
+#print "temp: "+str(json['currentTemp'])+'F |',
+#print "hour: "+str(json['hourSummary'])
 
 
 
